19bf0b4b4a6d657e7e449b4dfe9990f7ab8274ad
Serghei <pavlov.serghei@tutanota.com>
2023-08-25 18:15:19 +0200

    ex02, bonus

--- ex02/main.go
+++ ex02/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
+	_ "github.com/libsql/libsql-client-go/libsql"
 	"github.com/pserghei/gophercises/ex02/urlshort"
+	_ "modernc.org/sqlite"
 )
 
+var dbUrl = "file:urls.db"
+
 func main() {
+	db, err := sql.Open("libsql", dbUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", dbUrl, err)
+		os.Exit(1)
+	}
+
+	f := flag.String("f", "", "The yaml file to be opened")
+	flag.Parse()
+
 	mux := defaultMux()
 
 	pathsToUrls := map[string]string{
@@ -16,19 +32,53 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	yaml := `
+	yaml := make([]byte, 0)
+	if *f != "" {
+		var err error
+		yaml, err = os.ReadFile(*f)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		yaml = []byte(`
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
-`
+`)
+	}
 
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Starting the server on :8080")
+	json := []byte(`
+    [
+    {
+        "path": "/example",
+        "url": "https://example.com"
+    },
+    {
+        "path": "/gophercises",
+        "url": "https://gophercises.com"
+    }
+    ]
+    `)
+
+	jsonHandler, err := urlshort.JSONHandler(json, mapHandler)
+	if err != nil {
+		panic(err)
+	}
+
+	dbHandler, err := urlshort.DBHandler(db, mapHandler)
+	if err != nil {
+		panic(err)
+	}
+
+    fmt.Println("Starting servers on :8000, :8080 and :8888")
+	go http.ListenAndServe(":8000", jsonHandler)
+    go http.ListenAndServe(":8888", dbHandler)
 	http.ListenAndServe(":8080", yamlHandler)
 }
 
--- ex02/urlshort/handler.go
+++ ex02/urlshort/handler.go
@@ -1,10 +1,26 @@
 package urlshort
 
 import (
+	"database/sql"
+	json "encoding/json"
 	"net/http"
 
 	"gopkg.in/yaml.v2"
 )
+
+type urlS struct {
+	Path string `yaml:"path" json:"path"`
+	Url  string `yaml:"url" json:"url"`
+}
+
+func makeMap(s []urlS) map[string]string {
+	m := make(map[string]string)
+	for _, v := range s {
+		m[v.Path] = v.Url
+	}
+
+	return m
+}
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -18,22 +34,65 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	y := []struct {
-		Path string `yaml:"path"`
-		Url  string `yaml:"url"`
-	}{}
-
-	err := yaml.Unmarshal(yml, &y)
+func makeYAML(yml []byte) []urlS {
+	us := []urlS{}
+	err := yaml.Unmarshal(yml, &us)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
 
-	m := make(map[string]string)
+	return us
+}
 
-	for _, v := range y {
-		m[v.Path] = v.Url
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	us := makeYAML(yml)
+
+	return MapHandler(makeMap(us), fallback), nil
+}
+
+func makeJSON(j []byte) []urlS {
+	us := []urlS{}
+	err := json.Unmarshal(j, &us)
+	if err != nil {
+		panic(err)
 	}
 
-	return MapHandler(m, fallback), nil
+	return us
+}
+
+func JSONHandler(j []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	us := makeJSON(j)
+
+	return MapHandler(makeMap(us), fallback), nil
+}
+
+func readRows(rows *sql.Rows) []urlS {
+	us := []urlS{}
+
+	for rows.Next() {
+		url := urlS{}
+
+		err := rows.Scan(&url.Path, &url.Url)
+		if err != nil {
+			panic(err)
+		}
+
+		us = append(us, url)
+	}
+
+	return us
+}
+
+func DBHandler(db *sql.DB, fallback http.Handler) (http.HandlerFunc, error) {
+	stmt := "select * from urls"
+
+	rows, err := db.Query(stmt)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	us := readRows(rows)
+
+	return MapHandler(makeMap(us), fallback), nil
 }
